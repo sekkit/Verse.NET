e3ad133336773bc1a6b67805f6309685acb8e2c7
sekkit <sekkit@yandex.com>
2020-07-04 02:15:04 +0800

    bugfix

---
+++ src/Server.App/src/Bootstrap.cs
@@ -0,0 +1,16 @@
+﻿using Fenix;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.App
+{
+    public class Bootstrap
+    {
+        public static void Start()
+        {
+            var c = Container.Create("0.0.0.0", 7777);
+            ContainerHelper.Run(c);
+        }
+    }
+}
---
+++ src/Server.App/src/GModule/Match/MatchService.cs
@@ -0,0 +1,20 @@
+﻿using DotNetty.Codecs.Mqtt.Packets;
+using Fenix.FACG.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Fenix.Common.RpcUtil;
+
+namespace Fenix.FACG.GModule
+{
+    [ActorType(typeof(MatchService))]
+    public class MatchService: ActorLogic
+    { 
+        [ServerApi]
+        public void add_to_match(string uid, int match_type, Action<MatchCode> callback)
+        {
+
+            callback(MatchCode.OK);
+        }
+    }
+}
