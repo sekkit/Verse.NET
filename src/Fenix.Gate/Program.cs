ba82fc8f531f7d6260a494ed2313f73e6e2dc976
kitsek <i@sekkit.com>
2020-07-14 23:59:31 +0800

    remove bin obj

--- src/Fenix.Gate/Program.cs
+++
@@ -1,1 +0,0 @@
-﻿using System;namespace Fenix.Gate{    class Program    {        static void Main(string[] args)        {            var gateContainer = Container.Create("", 1111);            ContainerHelper.Run(gateContainer);         }    }}
