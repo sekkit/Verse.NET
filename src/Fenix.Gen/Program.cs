6f7eb032f43a7b8971104a81d993ba7854b30007
kitsek <i@sekkit.com>
2020-07-15 00:00:20 +0800

    add gen proj

---
+++ src/Fenix.Gen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Fenix.Gen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootFolder = Directory.GetCurrentDirectory();
+            var path = File.ReadAllBytes(Path.Combine(rootFolder, "Server.App.dll"));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
