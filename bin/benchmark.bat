c8a495bca14202f1dc94d01645b0cf6ad17d88ea
kitsek <i@sekkit.com>
2020-07-27 03:23:21 +0800

    missing files

---
+++ bin/benchmark.bat
@@ -0,0 +1,3 @@
+for /l %%i in (1,1,100) do (
+	call start_client.bat
+)
---
+++ bin/start_client.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set cwdpath=%~dp0
+set curpath=%cwdpath%..
+
+set CLIENT_PATH=%curpath%/src/Client.App/bin/Debug/netcoreapp3.1/Client.App.exe
+
+start %CLIENT_PATH%
