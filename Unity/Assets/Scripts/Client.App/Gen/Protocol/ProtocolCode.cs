cb622f8809ddef74570db55483dcbeae8300a461
kitsek <i@sekkit.com>
2020-07-29 02:20:37 +0800

    resolve protocode conflicts

---
+++ Unity/Assets/Scripts/Client.App/Gen/Protocol/ProtocolCode.cs
@@ -0,0 +1,32 @@
+﻿
+//AUTOGEN, do not modify it!
+
+using Fenix.Common;
+using Fenix.Common.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+namespace Shared
+{
+    public partial class ProtocolCode
+    {
+        public void Validate()
+        {
+            Dictionary<uint, string> dic = new Dictionary<uint, string>();
+            foreach(var f in this.GetType().GetFields())
+            {
+                uint key = (uint)f.GetValue(this);
+                if(dic.ContainsKey(key))
+                {
+                    Log.Info(string.Format("Duplicated protocol {0} and {1}", dic[key], f.Name));
+                }
+                dic[key] = f.Name;
+                Log.Info(string.Format("    ProtocolCode({0} = {1})", f.Name, f.GetValue(this)));
+            }
+
+            Log.Info(dic.Count() == this.GetType().GetFields().Count()?"Validation Passed": "Duplicated Protocol");
+        }
+    }
+}
+
