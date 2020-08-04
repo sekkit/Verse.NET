81f74d5ddc8af0c1070d4171c2dae41459750293
sekkitshi <seckkit@gmail.com>
2020-08-04 17:48:40 +0800

    NLog unity integration

---
+++ Unity/Assets/Plugins/Fenix/UnityCommon/IOUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unity.Common
+{
+    public class IOUtil
+    {
+        public static string persistentDataPath => UnityEngine.Application.persistentDataPath;
+    }
+}
