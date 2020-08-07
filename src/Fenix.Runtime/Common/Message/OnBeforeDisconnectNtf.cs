bd353fd246e7d352e0b7b1dca7676e8164976494
kitsek <i@sekkit.com>
2020-08-08 04:16:12 +0800

    missing files

---
+++ src/Fenix.Runtime/Common/Message/OnBeforeDisconnectNtf.cs
@@ -0,0 +1,51 @@
+﻿//AUTOGEN, do not modify it!
+
+using Fenix.Common;
+using Fenix.Common.Attributes;
+using Fenix.Common.Rpc;
+using MessagePack; 
+using System.ComponentModel;
+using System; 
+
+namespace Fenix.Common.Message
+{
+    [MessageType(OpCode.ON_BEFORE_DISCONNECT_NTF)]
+    [MessagePackObject]
+    public class OnBeforeDisconnectNtf : IMessageWithCallback
+    {
+        [Key(0)]
+        public DisconnectReason reason { get; set; }
+
+        [Key(1)]
+
+        public Callback callback
+        {
+            get => _callback as Callback;
+            set => _callback = value;
+        } 
+
+        [MessagePackObject]
+        public class Callback : IMessage
+        {
+
+            public override byte[] Pack()
+            {
+                return MessagePackSerializer.Serialize<Callback>(this);
+            }
+            public new static Callback Deserialize(byte[] data)
+            {
+                return MessagePackSerializer.Deserialize<Callback>(data);
+            }
+        }
+
+        public override byte[] Pack()
+        {
+            return MessagePackSerializer.Serialize<OnBeforeDisconnectNtf>(this);
+        }
+        public new static OnBeforeDisconnectNtf Deserialize(byte[] data)
+        {
+            return MessagePackSerializer.Deserialize<OnBeforeDisconnectNtf>(data);
+        }
+    }
+}
+
