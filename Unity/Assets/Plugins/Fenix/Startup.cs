7f778dcabc1595ba965dec056268559141625286
sekkitshi <seckkit@gmail.com>
2020-08-12 15:53:55 +0800

    add missing files

---
+++ Unity/Assets/Plugins/Fenix/Startup.cs
@@ -0,0 +1,30 @@
+﻿using DotNetty.Common.Utilities;
+using Fenix.Common.Message;
+using Fenix.Common.Rpc;
+using Fenix.Common.Utils;
+using MessagePack;
+using MessagePack.Resolvers;
+using System;
+using UnityEngine; 
+
+public class Startup
+{
+    static bool serializerRegistered = false;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void Initialize()
+    {
+        UnitySystemConsoleRedirector.Redirect();
+    }
+
+#if UNITY_EDITOR
+
+
+    [UnityEditor.InitializeOnLoadMethod]
+    static void EditorInitialize()
+    {
+        Initialize();
+    }
+
+#endif
+}
