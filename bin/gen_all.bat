839930bfa5d6ee013316b24348b632c822dcfdb2
sekkit <sekkit@yandex.com>
2020-08-14 03:22:10 +0800

    up

--- bin/gen_all.bat
+++ bin/gen_all.bat
@@ -3,17 +3,19 @@
 set cwdpath=%~dp0
 set curpath=%cwdpath%..
 
+set MPCBIN=%curpath%/mpc/win/mpc.exe
+
 set CLIENT_PATH=%curpath%/src/Fenix.Gen/bin/Debug/netcoreapp3.1/Fenix.Gen.exe
 
 %CLIENT_PATH% -r %curpath%
 %CLIENT_PATH% -c %curpath%
 %CLIENT_PATH% -s %curpath%
 
-dotnet-mpc -i %curpath%\src\Client.App\Gen\Message\ -o %curpath%\src\Client.App\Gen\Message\Generated\ClientAppMsg.g.cs -r ClientAppResolver
-dotnet-mpc -i %curpath%\src\Fenix.Runtime\Common\ -o %curpath%\src\Client.App\Gen\Message\Generated\FenixRuntimeMsg.g.cs -r FenixRuntimeResolver
-dotnet-mpc -i %curpath%\src\Shared\ -o %curpath%\src\Client.App\Gen\Message\Generated\SharedMsg.g.cs -r SharedResolver
+%MPCBIN% -i %curpath%\src\Client.App\Gen\Message\ -o %curpath%\src\Client.App\Gen\Message\Generated\ClientAppMsg.g.cs -r ClientAppResolver
+%MPCBIN% -i %curpath%\src\Fenix.Runtime\Common\ -o %curpath%\src\Client.App\Gen\Message\Generated\FenixRuntimeMsg.g.cs -r FenixRuntimeResolver
+%MPCBIN% -i %curpath%\src\Shared\ -o %curpath%\src\Client.App\Gen\Message\Generated\SharedMsg.g.cs -r SharedResolver
 
-dotnet-mpc -i %curpath%\src\Client.App\Gen\Message\ -o %curpath%\src\Server.App\Gen\Message\Generated\ClientAppMsg.g.cs -r ClientAppResolver
-dotnet-mpc -i %curpath%\src\Fenix.Runtime\Common\ -o %curpath%\src\Server.App\Gen\Message\Generated\FenixRuntimeMsg.g.cs -r FenixRuntimeResolver
-dotnet-mpc -i %curpath%\src\Shared\ -o %curpath%\src\Server.App\Gen\Message\Generated\SharedMsg.g.cs -r SharedResolver
+%MPCBIN% -i %curpath%\src\Client.App\Gen\Message\ -o %curpath%\src\Server.App\Gen\Message\Generated\ClientAppMsg.g.cs -r ClientAppResolver
+%MPCBIN% -i %curpath%\src\Fenix.Runtime\Common\ -o %curpath%\src\Server.App\Gen\Message\Generated\FenixRuntimeMsg.g.cs -r FenixRuntimeResolver
+%MPCBIN% -i %curpath%\src\Shared\ -o %curpath%\src\Server.App\Gen\Message\Generated\SharedMsg.g.cs -r SharedResolver
 
