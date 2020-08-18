1631046f9bb729f2c756ae877f18a3a2ff9dcf73
sekkitshi <seckkit@gmail.com>
2020-08-18 20:34:18 +0800

    bugfix

--- bin/gen_all.bat
+++ bin/gen_all.bat
@@ -3,13 +3,13 @@
 set cwdpath=%~dp0
 set curpath=%cwdpath%..
 
-set MPCBIN=%curpath%/mpc/win/mpc.exe
+set MPCBIN=%cwdpath%/mpc/win/mpc.exe
 
 set CLIENT_PATH=%curpath%/src/Fenix.Gen/bin/Debug/netcoreapp3.1/Fenix.Gen.exe
 
 %CLIENT_PATH% -r %curpath%
 %CLIENT_PATH% -c %curpath%
-%CLIENT_PATH% -s %curpath%
+%CLIENT_PATH% -s %curpath% 
 
 %MPCBIN% -i %curpath%\src\Client.App\Gen\Message\ -o %curpath%\src\Client.App\Gen\Message\Generated\ClientAppMsg.g.cs -r ClientAppResolver
 %MPCBIN% -i %curpath%\src\Fenix.Runtime\Common\ -o %curpath%\src\Client.App\Gen\Message\Generated\FenixRuntimeMsg.g.cs -r FenixRuntimeResolver
