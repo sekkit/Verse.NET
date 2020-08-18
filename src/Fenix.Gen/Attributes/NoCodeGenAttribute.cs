2164507301346f2712588fbabddb54ecc21ac54c
sekkitshi <seckkit@gmail.com>
2020-08-18 20:34:56 +0800

    up

---
+++ src/Fenix.Gen/Attributes/NoCodeGenAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fenix.Common.Attributes
+{
+    public class NoCodeGenAttribute : Attribute
+    {
+        public NoCodeGenAttribute()
+        {
+        }
+    }
+}
