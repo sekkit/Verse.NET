aaf19e423890317bd52a787b7769e46e98b570e3
sekkitshi <seckkit@gmail.com>
2020-08-31 19:10:31 +0800

    up

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -5,12 +5,14 @@ export curpath=${cwdpath}/..
 
 export APP_CONF=${curpath}/conf/app.json
 
+export SERVER_CONF=${curpath}/conf/server.json
+
 export BIN_PATH=${cwdpath}/netcoreapp3.1
 
 echo ${BIN_PATH}/Server.App.dll
 
-nohup dotnet ${BIN_PATH}/Server.App.dll --Config=${APP_CONF} --AppName="Login.App" > /dev/null 2>&1 &
-nohup dotnet ${BIN_PATH}/Server.App.dll --Config=${APP_CONF} --AppName="Match.App" > /dev/null 2>&1 &
-nohup dotnet ${BIN_PATH}/Server.App.dll --Config=${APP_CONF} --AppName="Master.App" > /dev/null 2>&1 &
-nohup dotnet ${BIN_PATH}/Server.App.dll --Config=${APP_CONF} --AppName="Zone.App" > /dev/null 2>&1 &
+nohup dotnet ${BIN_PATH}/Server.App.dll --RuntimeConfig=${APP_CONF} --ServerConfig=${SERVER_CONF} --AppName="Login.App" > /dev/null 2>&1 &
+nohup dotnet ${BIN_PATH}/Server.App.dll --RuntimeConfig=${APP_CONF} --ServerConfig=${SERVER_CONF} --AppName="Match.App" > /dev/null 2>&1 &
+nohup dotnet ${BIN_PATH}/Server.App.dll --RuntimeConfig=${APP_CONF} --ServerConfig=${SERVER_CONF} --AppName="Master.App" > /dev/null 2>&1 &
+nohup dotnet ${BIN_PATH}/Server.App.dll --RuntimeConfig=${APP_CONF} --ServerConfig=${SERVER_CONF} --AppName="Zone.App" > /dev/null 2>&1 &
 
