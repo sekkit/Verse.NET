aff21eadf7fbef4b3630e8835fa5f4b6dca2ddc0
sekkitshi <seckkit@gmail.com>
2020-08-31 19:09:25 +0800

    up

--- bin/start_server.bat
+++ bin/start_server.bat
@@ -6,11 +6,13 @@ set curpath=%cwdpath%..
 
 set APP_CONF=%curpath%/conf/app.json
 
+set SERVER_CONF=%curpath%/conf/server.json
+
 set BIN_PATH=%cwdpath%/netcoreapp3.1
 
 echo %BIN_PATH%/Server.App.dll
 
-start %BIN_PATH%/Server.App.exe --Config=%APP_CONF% --AppName="Login.App"
-start %BIN_PATH%/Server.App.exe --Config=%APP_CONF% --AppName="Match.App"
-start %BIN_PATH%/Server.App.exe --Config=%APP_CONF% --AppName="Master.App"
-start %BIN_PATH%/Server.App.exe --Config=%APP_CONF% --AppName="Zone.App"
+start %BIN_PATH%/Server.App.exe --RuntimeConfig=%APP_CONF% --ServerConfig=%SERVER_CONF% --AppName="Login.App"
+start %BIN_PATH%/Server.App.exe --RuntimeConfig=%APP_CONF% --ServerConfig=%SERVER_CONF% --AppName="Match.App"
+start %BIN_PATH%/Server.App.exe --RuntimeConfig=%APP_CONF% --ServerConfig=%SERVER_CONF% --AppName="Master.App"
+start %BIN_PATH%/Server.App.exe --RuntimeConfig=%APP_CONF% --ServerConfig=%SERVER_CONF% --AppName="Zone.App"
