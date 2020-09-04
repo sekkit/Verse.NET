b1db109aff2a5e8cd28925251d4b945612ad4352
sekkitshi <seckkit@gmail.com>
2020-09-04 16:28:18 +0800

    add missing files

---
+++ src/Fenix.Runtime/Common/Message/SayHelloReq.cs
@@ -0,0 +1,69 @@
+﻿//AUTOGEN, do not modify it!
+
+using Fenix.Common.Utils;
+using Fenix.Common;
+using Fenix.Common.Attributes;
+using Fenix.Common.Rpc;
+using MessagePack; 
+using System.ComponentModel;
+using System; 
+
+namespace Fenix.Common.Message
+{
+    [MessageType(OpCode.SAY_HELLO_REQ)]
+    [MessagePackObject]
+    public class SayHelloReq : IMessageWithCallback
+    {
+
+        [Key(0)]
+
+        public Callback callback
+        {
+            get => _callback as Callback;
+            set => _callback = value;
+        } 
+
+        [MessagePackObject]
+        public class Callback : IMessage
+        {
+            [Key(0)]
+            public global::Fenix.Common.DefaultErrCode code { get; set; } = DefaultErrCode.ERROR;
+
+            [Key(1)]
+            public global::Fenix.HostInfo arg1 { get; set; }
+
+            public override byte[] Pack()
+            {
+                return MessagePackSerializer.Serialize<Callback>(this);
+            }
+
+            public new static Callback Deserialize(byte[] data)
+            {
+                return MessagePackSerializer.Deserialize<Callback>(data);
+            }
+
+            public override void UnPack(byte[] data)
+            {
+                var obj = Deserialize(data);
+                Copier<Callback>.CopyTo(obj, this);
+            }
+        }
+
+        public override byte[] Pack()
+        {
+            return MessagePackSerializer.Serialize<SayHelloReq>(this);
+        }
+
+        public new static SayHelloReq Deserialize(byte[] data)
+        {
+            return MessagePackSerializer.Deserialize<SayHelloReq>(data);
+        }
+
+        public override void UnPack(byte[] data)
+        {
+            var obj = Deserialize(data);
+            Copier<SayHelloReq>.CopyTo(obj, this);
+        }
+    }
+}
+
